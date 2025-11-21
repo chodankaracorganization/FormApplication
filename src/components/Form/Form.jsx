cc79ee169fd9542b0d1fb341f7cdd04b1d800d35
ashish.chodankar <ashish.chodankar@squareyards.co.in>
2025-11-21 09:57:15 +0530

    Form validations added part 2

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,8 +1,15 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import validatePassword from "../../helper/passwordValidator";
 import validateEmail from "../../helper/emailValidator";
 
 function Form() {
+  const exampleRef = useRef(0);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(exampleRef);
+  }, [count]);
+
   const [formValues, setFormValues] = useState({
     email: "ab@cd.com",
     password: "",
@@ -10,6 +17,8 @@ function Form() {
 
   const handleValidatePassword = () => {
     const password = formValues.password;
+    document.getElementById("password-input").focus();
+
     if (!validatePassword(password)) {
       console.log("password doesn't contain required parameters");
     }
@@ -18,21 +27,27 @@ function Form() {
     event.preventDefault();
     hadleValidateEmail();
     handleValidatePassword();
+    document.getElementById("email-input").focus();
     console.log(formValues);
   };
 
   const hadleValidateEmail = (event) => {
     const email = formValues.email();
+    document.getElementById("email-input").focus();
     if (!validateEmail(email)) {
       console.log("email doesn't contain required parameters");
     }
   };
   return (
     <div>
-      New Form
+      New Form <br /> Count:{count}
+      ExampleRef:{exampleRef.current} <br />
+      <button onClick={() => setCount(count + 1)}>update count</button>
+      <button onClick={() => exampleRef.current++}>update ref</button>
       <form onSubmit={handleFormSubmit}>
         <div className="email-input-wrapper" type="email">
           <input
+            id="email-input"
             type="text"
             value={formValues.email}
             onChange={(event) =>
@@ -42,6 +57,7 @@ function Form() {
         </div>
         <div className="password-input-wrapper" type="password">
           <input
+            id="password-input"
             type="password"
             value={formValues.password}
             onChange={(event) =>
